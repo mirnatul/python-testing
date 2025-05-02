1b69292a3c6a0e0ebe96b6ea42284dc56581e411
mirnatul <mirnatul@gmail.com>
2025-05-02 11:21:28 +0600

    commit all

---
+++ 003_split_test.py
@@ -0,0 +1,4 @@
+text = "Hello there, I am using whatsapp"
+
+print(text.split()) # by whitespace
+print(text.split(",")) # by specific item
---
+++ 008_oop.py
@@ -0,0 +1,6 @@
+# creation of a class
+class MyClass:
+    x = 5
+# creation of an object
+p1 = MyClass()
+print(p1.x)
