3a885c7f659795b36428bb9c0be05b3a9c918623
mirnatul <mirnatul@gmail.com>
2025-05-04 02:54:25 +0600

    polymorphism added

---
+++ 014_polymorphism.py
@@ -0,0 +1,23 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Drive!")
+
+
+class Boat:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        print("Sail!")
+
+
+car1 = Car("Ford", "Mustang")
+boat1 = Boat("Ibiza", "Touring 20")
+
+for i in [car1, boat1]:
+    i.move()
